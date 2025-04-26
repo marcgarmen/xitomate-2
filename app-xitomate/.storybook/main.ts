d0618c79733fb96d1f85db8745d5e26cd7e1091d
Marco <marcoagarciam8@gmail.com>
2025-04-25 20:11:48 -0600

    resolucion cambios

--- app-xitomate/.storybook/main.ts
+++ app-xitomate/.storybook/main.ts
@@ -1,21 +1,22 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: [
+  "stories": [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-essentials",
-    "@storybook/addon-links"
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
   ],
   "framework": {
     "name": "@storybook/nextjs",
     "options": {}
   },
-  staticDirs: [
+  "staticDirs": [
     "../public"
   ]
 };
-
 export default config;
